a14da19f09f72481813d73cc69377237bff40ae7
Yiye Chen <yychen2019@gatech.edu>
2021-08-14 17:37:38 -0400

    finish the hand move

--- Surveillance/activities/simulator.py
+++ Surveillance/activities/simulator.py
@@ -5,9 +5,12 @@
     @date               08/14/2021
 
 """
+from Surveillance.activities.state_parse import parse_progress
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
+import math
+
 from dataclasses import dataclass
 
 @dataclass
@@ -40,6 +43,7 @@ class Puzzle_Piece():
 
         # state
         self.assembled = False
+        self.picked = False
 
     def draw(self, ax):
         # the xy parameter is the left bottom corner
@@ -56,7 +60,7 @@ class Hand():
     The class for hand controlling.
     Hardcode the hand as a circle
     """
-    def __init__(self, radius=5, color='r'):
+    def __init__(self, radius=5, color='r', speed=5):
 
         # circle radius & centroid location
         self.location = Location()
@@ -66,10 +70,29 @@ class Hand():
         self.color = 'r'
 
         # a piece in hand?
-        self.piece_in_hand = None
+        self.pieces_in_hand = None
+
+        # speed
+        self.speed = speed
     
-    def move(self):
-        pass
+    def move(self, target:Location, delta_t):
+        """
+        If cannot reach the target within the time interval, then move to the furthest
+        If can, then stop at the target
+        """
+        # distance
+        delta_x = target.x - self.location.x
+        delta_y = target.y - self.location.y
+        distance = math.sqrt(delta_x**2 + delta_y**2)
+
+        if distance >= delta_t * self.speed:
+            x_step = delta_x * (delta_t * self.speed) / distance
+            y_step = delta_y * (delta_t * self.speed) / distance
+            self.location.x += x_step
+            self.location.y += y_step
+        else:
+            self.location.x = target.x
+            self.location.y = target.y
 
     def pick(self, piece):
         pass
@@ -90,21 +113,25 @@ class Simulator():
     @param[in] N_piece          the number of puzzle pieces
     @param[in] size             The canvas size
     """
-    def __init__(self, N_piece=1, size=100) -> None:
+    def __init__(self, N_piece=1, size=100, speed=5):
 
         self.size = size
+        self.speed = speed
 
         # initialize the piece position and the target piece position
-        self.piece = [] 
+        self.pieces = [] 
         self.target_locations = []
         for i in range(N_piece):
-            self.piece.append(Puzzle_Piece(size=15, color='g'))
-            self.piece[i].location.x = int(3/4*self.size)
-            self.piece[i].location.y = int((i+1) / (N_piece+1) * self.size)
-            self.target_locations.append(Location(int(1/4*self.size), self.piece[i].location.y))
+            self.pieces.append(Puzzle_Piece(size=15, color='g'))
+            self.pieces[i].location.x = int(3/4*self.size)
+            self.pieces[i].location.y = int((i+1) / (N_piece+1) * self.size)
+            self.target_locations.append(Location(int(1/4*self.size), self.pieces[i].location.y))
+
+        # the observable pieces
+        self.pieces_on_table = self._get_pieces_on_table()
         
         # initialize the hand position
-        self.hand = Hand()
+        self.hand = Hand(speed = speed)
         self.hand.location.x = int(self.size/2)
         self.hand.location.y = int(self.size/2)
 
@@ -114,23 +141,45 @@ class Simulator():
         @param[in]  delta_t         The time length for one simulation step
         @param[out] finish          Binary. Finished or not
         """
+        # find the next piece to deal with
+        for piece in self.pieces:
+            if not piece.assembled:
+                break
+        
+        if True:
+            # reach to a piece to pick
+            self.hand.move(piece.location, delta_t)
+        elif False:
+            # pick up a piece
+            self.hand.pick(piece)
+        elif False:
+            # reach to a target position
+            target = None
+            self.hand.move(target)
+        elif False:
+            # place the piece
+            self.hand.place(piece)
+
         return False
 
     def draw(self, ax):
         # draw pieces
-        for i in range(len(self.piece)):
-            self.piece[i].draw(ax)
+        for i in range(len(self.pieces)):
+            self.pieces[i].draw(ax)
         
         # draw the hand
         self.hand.draw(ax)
+    
+    def _get_pieces_on_table(self):
+        return [p for p in self.pieces if not p.picked]
 
 if __name__ == "__main__":
     # simulator
-    simulator = Simulator()
+    simulator = Simulator(speed=10)
 
     # simulation variables
     finish_flag = False
-    delta_t = 0.5
+    delta_t = 0.01
 
     # figure for the visualization
     fh, ax = plt.subplots()
@@ -142,7 +191,7 @@ if __name__ == "__main__":
         finish_flag = simulator.simulate_step(delta_t)
 
         # remove previous draw
-        for artist in plt.gca().lines + plt.gca().collections:
+        for artist in ax.patches:
             artist.remove()
         # draw the current step
         simulator.draw(ax)
