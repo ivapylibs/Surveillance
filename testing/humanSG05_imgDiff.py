79a4a009e719218366604cc6b06101b69c344559
Yiye Chen <yychen2019@gatech.edu>
2021-09-29 11:45:47 -0400

    Finish the humanSG05_imgDiff test

--- testing/humanSG05_imgDiff.py
+++ testing/humanSG05_imgDiff.py
@@ -23,42 +23,48 @@ import time
 import numpy as np
 
 import trackpointer.centroid as tracker
-from Surveillance.layers.human_seg import Human_ColorSG_HeightInRange
+from Surveillance.layers.human_seg import Human_ColorSG_HeightInRange, Params
 
 fPath = os.path.realpath(__file__)
 tPath = os.path.dirname(fPath)
-dPath = os.path.join(tPath, 'data')
+dPath = os.path.join(tPath, 'data/puzzle_layer')
 
-# train data. Requires two training images:
-# the image of the indicator glove and the image of an empty table with the camera intrinsic parameter
-train_img_glove = cv2.imread(os.path.join(dPath, 'glove_small_0.png'))[:,:,::-1]
-train_depth_table = np.load(
-    os.path.join(dPath, "empty_desk_data_0.npz"),
+# train data.
+empty_table_rgb = cv2.imread(os.path.join(dPath, "empty_table_0.png"))[:,:,::-1]
+empty_table_depth = np.load(
+    os.path.join(dPath, "empty_table_data_0.npz"),
     allow_pickle=True
 )["depth_frame"]
+glove_rgb = cv2.imread(os.path.join(dPath, "calibrate_glove_0.png"))[:,:,::-1]
 intrinsic = np.load(
-    os.path.join(dPath, "empty_desk_data_0.npz"),
+    os.path.join(dPath, "empty_table_data_0.npz"),
     allow_pickle=True
 )["intrinsics"]
 
 # ======= [2] build the segmentor instance
 trackptr = tracker.centroid()
-human_seg = Human_ColorSG_HeightInRange.buildFromImage(train_img_glove, train_depth_table, intrinsic, tracker=trackptr)
-
-passthrough = lambda x: x
-takeInverse = lambda x: ~x
+human_seg = Human_ColorSG_HeightInRange.buildImgDiff(
+    empty_table_rgb, 
+    glove_rgb, 
+    empty_table_depth, 
+    intrinsic, 
+    tracker=trackptr, 
+    params=Params(
+        det_th=8
+    )
+)
 
 # ======= [3] test on teh test image and show the result
 # for each new test image, need to essentially create a new postprocess executable
 timing_list = []
-for i in range(6):
+for i in range(5):
 
     # prepare visualization
     fh, axes = plt.subplots(1, 3, figsize=(15, 5))
 
     # read data
-    rgb_path = os.path.join(dPath, "human_puzzle_small_{}.png".format(i))
-    depth_path = os.path.join(dPath, "human_puzzle_small_data_{}.npz".format(i))
+    rgb_path = os.path.join(dPath, "human_puzzle_{}.png".format(i))
+    depth_path = os.path.join(dPath, "human_puzzle_data_{}.npz".format(i))
     test_rgb = cv2.imread(rgb_path)[:,:,::-1]
     test_depth = np.load(depth_path, allow_pickle=True)['depth_frame']
 
@@ -77,20 +83,17 @@ for i in range(6):
     plt.imshow(test_rgb)
 
     plt.subplot(222)
+    plt.title("The estimated height map")
+    plt.imshow(human_seg.height_map)
+
+    plt.subplot(223)
     plt.title("The detection result from the Single-Gaussian detector")
     human_seg.draw_layer(img=test_rgb, raw_detect=True)
 
-    plt.subplot(223)
+    plt.subplot(224)
     plt.title("The final result - after lower region removal")
     human_seg.draw_layer(img=test_rgb)
 
-    # test the customized post process
-    human_seg.update_postprocess(takeInverse)
-    human_seg.process(test_rgb)
-    plt.subplot(224)
-    plt.title("The fool customized postprocessor to take the invert")
-    human_seg.draw_layer(img=test_rgb)
-    human_seg.update_postprocess(passthrough)
 
 print("\n\n The average processing time for each test frame: {} sec/frame \n\n".format(np.mean(timing_list)))
 plt.show()
