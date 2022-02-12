10a8ce013dc7ea985e2c163b3c1837f0a08985eb
Yiye Chen <yychen2019@gatech.edu>
2022-02-12 11:40:21 -0500

    Move state parsing

--- Surveillance/activities/state.py
+++ Surveillance/activities/state.py
@@ -7,7 +7,7 @@
     @date               08/12/2021
 
 """
-
+from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -39,21 +39,24 @@ class Base_state(Base):
         self.state_names = state_names
         self.signal_names = signal_names
 
-        self.signals_cache = np.empty((self.signal_number, self.signal_cache_limit))       # a list of the cached signal list, each element of which represents a signal
+        # signal cache - 2d list of the shape (self.signal_number, self.signal_cache_limit)
+        self.signals_cache = []
+        for i in range(self.signal_number):
+            self.signals_cache.append([])
+        
+        # state cache - since the current design are binary, make it array
         self.states_cache = np.empty((self.state_number, self.state_cache_limit))        # a list of the cached state list, each element of which represents a state 
 
         # store the figure handle index for display
         self.f_idx = None
 
-    def measure(self, cur_signals):
+    def measure(self, cur_signals:List):
         """
         The workflow of the signal2state 
 
         @param[in]  cur_signals.            An array of the signals, each element of which represents the new income of different signal types
         """
-        if isinstance(cur_signals, list):
-            cur_signals = np.array(cur_signals)
-        assert cur_signals.size == self.signal_number
+        assert len(cur_signals) == self.signal_number
 
         cur_states = self.parse(cur_signals)
         assert cur_states.size == self.state_number
@@ -65,7 +68,7 @@ class Base_state(Base):
 
     def parse(self, cur_signals):
         """
-        Parse the state out of the signals
+        Parse the state out of the signals. Return the states
         """
         raise NotImplementedError
 
@@ -106,23 +109,92 @@ class Base_state(Base):
         """
         @param[in]  cur_count           The count BEFORE appendin the new
         """
-        if cur_count < num_limit:
-            cache[:, cur_count] = new
-        else:
-            cache[:, :num_limit-1] = cache[:, 1:num_limit]
-            cache[:, num_limit-1] = new
+        if isinstance(cache, np.ndarray):
+            assert isinstance(new, np.ndarray) and cache.shape[0] == new.shape[0]
+            if cur_count < num_limit:
+                cache[:, cur_count] = new
+            else:
+                cache[:, :num_limit-1] = cache[:, 1:num_limit]
+                cache[:, num_limit-1] = new
+        elif isinstance(cache, list):
+            assert isinstance(new, list) and len(cache) == len(new)
+            if cur_count < num_limit:
+                # iterate through the state/signal numbers
+                for i in range(len(cache)):
+                    cache[i].append(new[i])
+            else:
+                # iterate through the state/signal numbers
+                for i in range(len(cache)):
+                    # first pop out the first element then append new
+                    cache[i].pop(0)
+                    cache[i].append(new[i])
 
 class StateEstimator(Base_state):
     """
     The State Estimator v1.0
 
-    Fix to estimate three binary states: Move, Make_Puzzle_Progress, Puzzle_in_hand
-    For now allow customization for the signals to be used.
+    Fix to estimate three binary states: Move, Make_Puzzle_Progress, Puzzle_in_hand.
+    The signal used for the state estimation is:
+        1. The hand location,
+        2. The puzzle solving progress (percentage)
+        3. Piece in hand 
+    For now allow customization for the signals and the states used for the development purpose
     """
 
-    def __init__(self, signal_number, signal_cache_limit=1000, state_cache_limit=1000,signal_names=[]):
-        super().__init__(signal_number, state_number=3, signal_cache_limit=state_cache_limit, state_cache_limit=state_cache_limit,
-                        signal_names=signal_names, state_names=["Move", "Progress_Made", "Puzzle_in_Hand"])
+    def __init__(self, signal_number, signal_cache_limit=1000, state_cache_limit=1000,signal_names=[],\
+        state_number = 3, state_names=["Move", "Progress_Made", "Puzzle_in_Hand"],\
+        move_th=2):
+        super().__init__(signal_number, state_number=state_number, signal_cache_limit=signal_cache_limit, state_cache_limit=state_cache_limit,
+                        signal_names=signal_names, state_names=state_names)
+        
+        self.move_th = 2
 
-    def parse(self, signals):
-        pass
+    def parse(self, cur_signals:List):
+        """Parse the states out of the signals
+
+        Args:
+            cur_signals (np.ndarray, (signal_number, )):    The signals of the current timestamp. \
+                According to the current design, the signals should be a list who stores: \
+                    1. hand location (np.ndarray, (2, ))
+                    2. Puzzle solving progress (percentage number)
+                    3. puzzle piece in hand (binary)
+        Return:
+            cur_states (np.ndarray, (state_num, )):         The states parsed of the current timestamp
+        """
+        cur_states = np.zeros((self.state_number), dtype=bool)
+        
+        # parse move
+        cur_states[0] = self.parse_move(cur_signals)
+
+        return cur_states
+    
+    def parse_move(self, cur_signals):
+        """Parse the moving state
+        """
+
+        # if the first signal, just return not moving
+        if self.signal_cache_count == 0:
+            return False
+
+        # convention: The location is the first signal
+        cur_location = cur_signals[0]
+        last_location = self.signals_cache[0][-1]
+
+        distance = np.linalg.norm(
+            np.array(cur_location) - np.array(last_location)
+        )
+
+        return distance > self.move_th
+    
+    def parse_progress(self, cur_signals):
+        """
+        Parse the progress state, which is a binary indicator of whether the puzzle solving is making progress or not
+        """
+        raise NotImplementedError
+
+    def parse_PinH(self, cur_signals):
+        """
+        Parse whether the puzzle-in-hand state, which is a binary indicator whether the hand is holding a puzzle piece or not
+        """
+        raise NotImplementedError
+
