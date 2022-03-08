47c4018def577719598364bdc27c93f738c33c4a
Yunzhi Lin <yunzhi.lin@gatech.edu>
2022-03-08 17:19:17 -0500

    issue with callback

--- Surveillance/deployment/ROS/rosbag_runner.py
+++ Surveillance/deployment/ROS/rosbag_runner.py
@@ -39,6 +39,7 @@ fDir = "./"
 lock = threading.Lock()
 
 timestamp_ending = None
+roscore_proc = None
 flag_FINISHED = False
 
 # To be built
@@ -58,66 +59,113 @@ def get_args():
     args = parser.parse_args()
     return args
 
-def callback(arg_list):
+class ImageListener:
+    def __init__(self):
 
-    print("Get to the callback")
-    with lock:
-        RGB_np = arg_list[0]
-        D_np = arg_list[1]
-        timestamp = arg_list[2].to_sec()
+        # Data captured
+        self.RGB_np = None
+        self.D_np = None
 
-        # np.integer include both signed and unsigned, whereas the np.int only include signed
-        if np.issubdtype(D_np.dtype, np.integer):
-            print("converting the depth scale")
-            D_np = D_np.astype(np.float32) * surv.depth_scale
 
-        print("Running the Surveillance on the test data")
-        surv.process(RGB_np, D_np)
+        rospy.init_node("test_surveillance_on_rosbag")
 
-        hImg = surv.humanImg
-        pImg = surv.puzzleImg
+        # == [0] build from the rosbag
+        configs = bParams(
+            visualize=False,
+            ros_pub=False,
+            # The calibration topics
+            # deployment
+            BEV_mat_topic="BEV_mat",
+            intrinsic_topic="intrinsic",
+            # scene interpreter
+            empty_table_rgb_topic="empty_table_rgb",
+            empty_table_dep_topic="empty_table_dep",
+            glove_rgb_topic="glove_rgb",
+            human_wave_rgb_topic="human_wave_rgb",
+            human_wave_dep_topic="human_wave_dep",
+            depth_scale_topic="depth_scale"
+        )
+        self.surv = BaseSurveillanceDeploy.buildFromRosbag(rosbag_file, configs)
 
-        aa = surv.meaBoardImg
+        RGB_sub = Images_sub([test_rgb_topic, test_dep_topic],
+                             callback_np=self.callback)
+        print("Waiting for the data...")
 
-        # Display Below will also be stuck
-        #display_images_cv([hImg[:,:,::-1], pImg[:,:,::-1]], ratio=0.4, window_name="Surv Results")
-        #cv2.imshow("Surv Results", hImg)
-        #cv2.waitKey(1)
+    def callback(self, arg_list):
 
-        # Temporary. Save then out
-        print("Saving out the data")
-        ratio = 0.4
-        # images = [hImg[:,:,::-1], pImg[:,:,::-1]]
-        images = [RGB_np[:, :, ::-1]]
-        H, W = images[0].shape[:2]
-        if ratio is not None:
-           H_vis = int(ratio * H)
-           W_vis = int(ratio * W)
-        else:
-           H_vis = H
-           W_vis = W
-        images_vis = [cv2.resize(img, (W_vis, H_vis)) for img in images]
-        image_display = np.hstack(tuple(images_vis))
+        print("Get to the callback")
+        with lock:
+            self.RGB_np = arg_list[0]
+            self.D_np = arg_list[1]
+            self.timestamp = arg_list[2].to_sec()
+            print(self.timestamp)
+            # np.integer include both signed and unsigned, whereas the np.int only include signed
+            if np.issubdtype(self.D_np.dtype, np.integer):
+                print("converting the depth scale")
+                self.D_np = self.D_np.astype(np.float32) * self.surv.depth_scale
 
-        global call_back_num
-        cv2.imwrite("test_frame_{}.png".format(call_back_num), image_display)
+    def run_system(self):
 
-        call_back_num += 1
-        print("The processed test frame number: {} \n\n".format(call_back_num))
+        with lock:
+
+            if self.RGB_np is None:
+                print('asdasdasdasd')
+                return
+
+            print("Running the Surveillance on the test data")
+            self.surv.process(self.RGB_np, self.D_np)
+
+            hImg = self.surv.humanImg
+            pImg = self.surv.puzzleImg
+
+            print(self.timestamp)
+            # aa = self.surv.meaBoardImg
+
+            # # Display Below will also be stuck
+            # # display_images_cv([hImg[:,:,::-1], pImg[:,:,::-1]], ratio=0.4, window_name="Surv Results")
+            # # cv2.imshow("Surv Results", hImg)
+            # # cv2.waitKey(1)
+            #
+
+
+            # # Temporary. Save then out
+            # print("Saving out the data")
+            # ratio = 0.4
+            # # images = [hImg[:,:,::-1], pImg[:,:,::-1]]
+            # images = [self.RGB_np[:, :, ::-1]]
+            # H, W = images[0].shape[:2]
+            # if ratio is not None:
+            #     H_vis = int(ratio * H)
+            #     W_vis = int(ratio * W)
+            # else:
+            #     H_vis = H
+            #     W_vis = W
+            # images_vis = [cv2.resize(img, (W_vis, H_vis)) for img in images]
+            # image_display = np.hstack(tuple(images_vis))
+            #
+            # global call_back_num
+            # cv2.imwrite("test_frame_{}.png".format(call_back_num), image_display)
+            #
+            # call_back_num += 1
+            # print("The processed test frame number: {} \n\n".format(call_back_num))
 
         global timestamp_ending
         global flag_FINISHED
-
+        global roscore_proc
         # # Debug only
         # print('Current:', timestamp)
         # print('Last:', timestamp_ending)
 
-        if timestamp_ending is not None and abs(timestamp - timestamp_ending)<0.1:
+        if timestamp_ending is not None and abs(self.timestamp - timestamp_ending) < 0.1:
             flag_FINISHED = True
             print(flag_FINISHED)
 
         if flag_FINISHED is True:
             rospy.signal_shutdown('Finished')
+
+        # == [4] Stop the roscore if started from the script
+        if roscore_proc is not None:
+            terminate_process_and_children(roscore_proc)
 
 if __name__ == "__main__":
 
@@ -126,31 +174,13 @@ if __name__ == "__main__":
     rosbag_file = os.path.join(fDir, args.rosbag_name)
 
     # start the roscore if necessary
-    roscore_proc = None
     if not rosgraph.is_master_online():
         roscore_proc = subprocess.Popen(['roscore'])
         # wait for a second to start completely
         time.sleep(1)
-    rospy.init_node("test_surveillance_on_rosbag")
 
-    # == [0] build from the rosbag
-    configs = bParams(
-        visualize=False,
-        ros_pub=False,
-        # The calibration topics
-        # deployment
-        BEV_mat_topic="BEV_mat",
-        intrinsic_topic="intrinsic",
-        # scene interpreter
-        empty_table_rgb_topic="empty_table_rgb",
-        empty_table_dep_topic="empty_table_dep",
-        glove_rgb_topic="glove_rgb",
-        human_wave_rgb_topic="human_wave_rgb",
-        human_wave_dep_topic="human_wave_dep",
-        depth_scale_topic="depth_scale"
-    )
-    surv = BaseSurveillanceDeploy.buildFromRosbag(rosbag_file, configs)
-    
+    listener = ImageListener()
+
     # Debug only
     # == [1] Prepare the subscribers, and release the test data from the bag
     # check the data - The data has no problem
@@ -178,10 +208,6 @@ if __name__ == "__main__":
 
     # exit()
 
-    RGB_sub = Images_sub([test_rgb_topic, test_dep_topic],
-                         callback_np=callback)
-    print("Waiting for the data...")
-
     # Get basic info
     info_dict = yaml.safe_load(
         subprocess.Popen(['rosbag', 'info', '--yaml', rosbag_file], stdout=subprocess.PIPE).communicate()[0])
@@ -200,24 +226,24 @@ if __name__ == "__main__":
     # bag.close()
     # # exit()
 
-    # need to slow down the publication or the subscriber won't be able to catch it
-    command = "rosbag play {} -d 2 -r 0.5 --topic {} {}".format(
-       rosbag_file, test_rgb_topic, test_dep_topic)
-
-    try:
-       # Be careful with subprocess, pycharm needs to start from the right terminal environment (.sh instead of shortcut)
-       # https://stackoverflow.com/a/3478415
-       subprocess.call(command, shell=True)
-    except:
-       print("Cannot execute the bash command: \n {}".format(command))
-       exit()
+    # # Need to start later
+    # # need to slow down the publication or the subscriber won't be able to catch it
+    # command = "rosbag play {} -d 5 -r 1 -s 110 --topic {} {}".format(
+    #    rosbag_file, test_rgb_topic, test_dep_topic)
+    #
+    # try:
+    #    # Be careful with subprocess, pycharm needs to start from the right terminal environment (.sh instead of shortcut)
+    #    # https://stackoverflow.com/a/3478415
+    #    subprocess.call(command, shell=True)
+    # except:
+    #    print("Cannot execute the bash command: \n {}".format(command))
+    #    exit()
 
     while not rospy.is_shutdown():
 
         # Put processing & display here
+        listener.run_system()
 
-        rospy.spin()
-
-    # == [4] Stop the roscore if started from the script
-    if roscore_proc is not None:
-        terminate_process_and_children(roscore_proc)
+    # # == [4] Stop the roscore if started from the script
+    # if roscore_proc is not None:
+    #     terminate_process_and_children(roscore_proc)
