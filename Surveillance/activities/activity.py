e890ec8baccacd7a06af693a3a90887f3e8e2279
yunzhil <yunzhi.lin@gatech.edu>
2022-03-26 10:39:44 -0400

    clean up

--- Surveillance/activities/activity.py
+++
@@ -1,8 +0,0 @@
-"""
-
-    @brief              The classes of taking the state sequnces and interpret them into the activities.
-
-    @author             Yiye Chen.                  yychen2019@gatech.edu
-    @date               08/12/2021
-
-"""
--- Surveillance/activities/activity_parse.py
+++
@@ -1,8 +0,0 @@
-"""
-
-    @ brief                     A collection of activity parsing methods. The activity is parsed out of the states
-
-    @ author                    Yiye Chen.              yychen2019@gatech.edu
-    @ date                      08/13/2021  
-
-"""
