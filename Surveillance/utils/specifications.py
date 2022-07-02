9246b5d9d71362855eec9efbd6c5ef6381ebc055
Patricio A. Vela <pvela@gatech.edu>
2022-07-02 14:08:27 -0400

    rbreplay updated. surveillance yaml specs updated. rbrun OK.

--- Surveillance/utils/specifications.py
+++ Surveillance/utils/specifications.py
@@ -14,18 +14,85 @@
 #============================= specifications ============================
 
 import os
+from benedict import benedict
+
 
 class dict2struct:
   def __init__(self, **entries):
     self.__dict__.update(entries)
 
+  #============================ __contains__ ===========================
+  #
+  # @brief  Overload the "in" operator to request whether the class has
+  #         the targeted member variable.
+  #
+  # @param[in]  att_name    The member variable (attribute name).
+  #
+  def __contains__(self, att_name):
+    return hasattr(self, att_name)
+
+
 class specifications:
   def __init__(self, entries):
     self.source  = dict2struct(**entries['source'])
     self.general = dict2struct(**entries['general'])
-    self.exec    = dict2struct(**entries['exec'])
+    self.module  = dict2struct(**entries['module'])
+    self.output  = dict2struct(**entries['output'])
 
-    self.source.rosbag = os.path.expanduser(self.source.rosbag)
-    self.source.puzzle = os.path.expanduser(self.source.puzzle)
+    # Process surveillance system configuration
+    #
+    if 'surveillance' in entries:
+      print('yup, has surveillance')
 
-    self.exec.read_activity = False
+    self.surveillance  = dict2struct(**entries['surveillance'])
+
+    # Process activity parsing module configuration.
+    #
+    if 'activity' in entries:
+      print('Parsing activity parsing settings.')
+      self.activity = dict2struct(**entries['activity'],
+                                  **{'read': False})
+    else:
+      self.activity = dict2struct(**{'read': False})
+    # @todo Figure out what "read" is really trying to do and
+    #       whether it belongs in the configuration file.
+
+    # Process activity analysis module configuration.
+    #
+    if 'analysis' in entries:
+      print('Parsing activity analysis settings.')
+      self.analysis = dict2struct(**entries['analysis'])
+
+    # Process puzzle solver module configuration.
+    #
+    if 'puzzle' in entries:
+      print('Parsing activity analysis settings.')
+      self.puzzle = dict2struct(**entries['puzzle'])
+
+    # Expand "~" if found in the source paths.
+    #
+    if 'rosbag' in self.source:
+      self.source.rosbag = os.path.expanduser(self.source.rosbag)
+    if 'puzzle' in self.source:
+      self.source.puzzle = os.path.expanduser(self.source.puzzle)
+
+
+
+
+  #============================ __contains__ ===========================
+  #
+  # @brief  Overload the "in" operator to request whether the class has
+  #         the targeted member variable.
+  #
+  # @param[in]  att_name    The member variable (attribute name).
+  #
+  def __contains__(self, att_name):
+    return hasattr(self, att_name)
+
+  @staticmethod
+  def load(yfile):
+    ystr = benedict.from_yaml(yfile)    # load yaml file.
+    print(ystr)
+    conf = specifications(ystr)         # constructor specifications instance.
+    return conf
+
