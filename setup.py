e9575f1c75580bde9b199b8c0d3571fa6f7b84f4
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-02-17 15:09:04 -0500

    Use find_packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="Surveillance",
     version="1.1.0",
     description="Classes implementing a Surveillance system for the SuperviseIt project",
     author="IVALab",
-    packages=["Surveillance"],
+    packages=find_packages(),
     install_requires=[
         "psutil",
         "transitions",
