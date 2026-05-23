ea1e27c729264e23f8ba160280276a0360c844c8
Patricio A. Vela <pvela@gatech.edu>
2026-05-23 16:15:28 -0400

    Adding setup.py / requirements for ivapy code

--- setup.py
+++ setup.py
@@ -11,15 +11,5 @@ setup(
         "transitions",
         "python-benedict",
         "yacs",
-        "Lie @ git+https://github.com/ivapylibs/Lie.git",
-        "camera @ git+https://github.com/ivapylibs/camera.git",
-        "ROSWrapper @ git+https://github.com/ivaROS/ROSWrapper.git",
-        "improcessor @ git+https://github.com/ivapylibs/improcessor.git",
-        "trackpointer @ git+https://github.com/ivapylibs/trackpointer.git",
-        "detector @ git+https://github.com/ivapylibs/detector.git",
-        "perceiver @ git+https://github.com/ivapylibs/perceiver.git",
-        # NOTE: is this package necessary?
-        "puzzleSolvers @ git+https://github.com/ADCLab/puzzleSolvers.git",
-        "puzzle @ git+https://github.com/ivapylibs/puzzle_solver.git",
     ],
 )
